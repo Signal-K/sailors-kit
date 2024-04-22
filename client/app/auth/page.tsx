8fd321e254ae3cd2541ce99785eadf0249565f2d
Liam Arbuckle <liam@skinetics.tech>
2024-04-22 17:46:47 +0200

    🔦🎮 ↝ Windows is shite. Confirmed

---
+++ client/app/auth/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSession } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import LoginPage from "@/auth/LoginModal";
+
+
+const Login = () => {
+    const session = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session) {
+            router.push('/test');
+        }
+    }, [session, router]);
+
+    return (
+        <div className='container' style={{ padding: '50px 0 100px 0' }}>
+            {!session ? (
+                <LoginPage />
+            ) : (
+                <p>Logged in</p>
+            )}
+        </div>
+    );
+};
+
+export default Login;
